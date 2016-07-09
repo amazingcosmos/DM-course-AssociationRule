b5fc697132de0158f34b60ac88f3290fba70260e
LiYi <amazingcosmos@163.com>
2016-07-09 16:26:19 +0800

    finish association rules and figure plot.

--- code/association_rule.py
+++ code/association_rule.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.style.use('ggplot')
+
 import apriori
 import preprocess
 
@@ -21,4 +26,31 @@ f, f_support = apriori.apriori(data_set, MIN_SUPPORT)
 
 # generate the rules
 rules = apriori.gen_rules(f, f_support, MIN_CONFIDENT, MIN_LIFT)
-print len(rules)
+
+# discard duplicate rules
+# if rule A's lhs and rhs is the subset of rule B's
+# and rule A's lift is less than B's
+discard_rules = []
+for i in range(len(rules)):
+    rule_a = rules[i]
+    for j in range(len(rules)):
+        if i == j or i in discard_rules or j in discard_rules:
+            continue
+        rule_b = rules[j]
+        if(rule_a['lhs'].issubset(rule_b['lhs']) and rule_a['rhs'].issubset(rule_b['rhs']) and rule_a['lift'] <= rule_b['lift']):
+            # print rule_a, '\n', rule_b, '\n\n'
+            discard_rules.append(i)
+# remove the duplicate rules and stroe the rest rules into 'strong_rules'
+strong_rules = []
+for i in range(len(rules)):
+    if i not in discard_rules:
+        strong_rules.append(rules[i])
+
+# build up the DataFrame of strong rules
+df_strong_rules = pd.DataFrame(strong_rules)
+
+# plot the scatter figure
+plot = df_strong_rules.plot.scatter(x = 'support', y = 'confident', c = 'lift', s = 100)
+fig = plot.get_figure()
+fig.savefig('../image/rules_scatter.jpg')
+
